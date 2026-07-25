c48126aa41d4185029056163d72c49bc20ae5185
Samyog Lamsal <samyoglamsal@outlook.com>
2026-07-25 10:11:09 -0400

    feat: add wmenu as a dmenu replacement

--- modules/dwl/config.def.h
+++ modules/dwl/config.def.h
@@ -116,7 +116,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "ghostty", NULL };
-static const char *menucmd[] = { "wmenu-run", NULL };
+static const char *menucmd[] = { "fuzzel", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: 2 -> at, etc. */
--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -36,6 +36,7 @@
       neovim
       tree
       ghostty
+      wmenu
     ];
   };
 
