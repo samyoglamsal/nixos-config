f853786f7f3499e34302dc1a2825a56e743f9859
Samyog Lamsal <samyoglamsal@outlook.com>
2026-09-07 16:48:34 -0400

    feat: add install script

---
+++ dotfiles/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This script symlinks the config files located here to $XDG_CONFIG_HOME
+
+if [[ -z "${XDG_CONFIG_HOME}" ]]; then
+    echo "XDG_CONFIG_HOME environment variable is not set, defaulting to ${HOME}/.config"
+    XDG_CONFIG_HOME="${HOME}/.config"
+fi
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${script_dir}"
+
+for dir in */; do
+    dir="${dir%/}"
+    
+    if [[ "${dir}" == ".git" ]]; then
+        continue 
+    elif [[ -d "${XDG_CONFIG_HOME}/${dir}" ]]; then
+        echo "${dir} already exists in ${XDG_CONFIG_HOME}, please remove the existing directory if you'd like to use this script"
+    else
+        echo "Creating soft link for ${dir} in ${XDG_CONFIG_HOME}"
+        ln -s "${script_dir}/${dir}" "${XDG_CONFIG_HOME}/${dir}"
+    fi
+done
