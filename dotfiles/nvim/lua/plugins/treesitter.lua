4f12302f82ca11675cdd8c96b25c780aa459ec61
Samyog Lamsal <samyoglamsal@outlook.com>
2026-09-07 16:50:19 -0400

    fix: remove config option from treesitter plugin

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -3,8 +3,4 @@ return {
   branch = "main",
   lazy = false,
   build = ":TSUpdate",
-  config = function ()
-    local ts = require("nvim-treesitter")
-    ts.install({ "go", "python", "markdown", "lua" })
-  end,
 }
